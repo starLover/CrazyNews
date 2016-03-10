4f31fc00be65a91557cbd99d07cf752f143b81f6
聂欣欣 <1498146887@qq.com>
2016-03-10 21:45:49 +0800

    功能的完善

---
+++ CrazyNews/classes/Main/Controllers/SerachViewController.h
@@ -0,0 +1,13 @@
+//
+//  SerachViewController.h
+//  CrazyNews
+//
+//  Created by wanghongxiao on 16/3/10.
+//  Copyright © 2016年 聂欣欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SerachViewController : UIViewController
+
+@end
---
+++ CrazyNews/classes/Main/Controllers/SerachViewController.m
@@ -0,0 +1,37 @@
+//
+//  SerachViewController.m
+//  CrazyNews
+//
+//  Created by wanghongxiao on 16/3/10.
+//  Copyright © 2016年 聂欣欣. All rights reserved.
+//
+
+#import "SerachViewController.h"
+
+@interface SerachViewController ()
+
+@end
+
+@implementation SerachViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
